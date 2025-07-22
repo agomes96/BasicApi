917a1fd40133ad4655ad93ae44a4d0cdfc618ee8
antonio <antoniofilipegomes96@gmail.com>
2025-07-22 14:57:32 +0100

    initial commit

---
+++ Data/BasicApiContext.cs
@@ -0,0 +1,48 @@
+﻿using BasicApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BasicApi.Data
+{
+    public class BasicApiContext : DbContext
+    {
+        public BasicApiContext(DbContextOptions<BasicApiContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Book>().HasData(
+                new Book
+                {
+                    Id = 1,
+                    Title = "Title1",
+                    Author = "Author1",
+                    YearPublished = 2000
+                },
+                new Book
+                {
+                    Id = 2,
+                    Title = "Title2",
+                    Author = "Author2",
+                    YearPublished = 2000
+                },
+                new Book
+                {
+                    Id = 3,
+                    Title = "Title3",
+                    Author = "Author3",
+                    YearPublished = 2002
+                },
+                 new Book
+                 {
+                     Id = 4,
+                     Title = "Title4",
+                     Author = "Author4",
+                     YearPublished = 2004
+                 }
+                );
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ Migrations/20250722133412_book data added.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BasicApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class bookdataadded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "Author", "Title", "YearPublished" },
+                values: new object[,]
+                {
+                    { 1, "Author1", "Title1", 2000 },
+                    { 2, "Author2", "Title2", 2000 },
+                    { 3, "Author3", "Title3", 2002 },
+                    { 4, "Author4", "Title4", 2004 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
